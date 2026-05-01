58bdcd1bcd9cd6a0f0f0a4b71269fde2fcb2ff67
pong-openclaw <pong.openclaw.sandbox@gmail.com>
2026-05-01 09:30:14 +0700

    update sw.js 2026-05-01 02:30

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker — Boss Business Hub
-const CACHE = 'bosshub-202604300225';
+const CACHE = 'bosshub-202605010230';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
